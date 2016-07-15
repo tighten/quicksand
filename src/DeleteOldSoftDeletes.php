0837fe5c3da92924d10df97a623d09104fb138f6
Benson Lee <benson.lee@gmail.com>
2016-07-15 12:18:48 -0700

    Rename class; code review refactoring and extractions

---
+++ src/DeleteOldSoftDeletes.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Quicksand;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class DeleteOldSoftDeletes extends Command
+{
+    protected $signature = 'quicksand:run';
+
+    protected $description = 'Force delete all soft deleted content older than X days';
+
+    public function handle()
+    {
+        $deletedRows = $this->deleteOldSoftDeletes()
+            ->reject(function ($numRowsDeleted) {
+                return $numRowsDeleted === 0;
+            });
+
+        $this->logAffectedRows($deletedRows);
+    }
+
+    private function deleteOldSoftDeletes()
+    {
+        $models = collect(config('quicksand.models'));
+        $daysBeforeDeletion = config('quicksand.days');
+
+        if (empty($daysBeforeDeletion)) {
+            return collect([]);
+        }
+
+        return $models->map(function ($modelConfig, $modelName) use ($daysBeforeDeletion) {
+            if (! is_array($modelConfig)) {
+                $modelName = $modelConfig;
+                $modelConfig = [];
+            }
+
+            return $this->deleteOldSoftDeletesForModel($modelName, $modelConfig, $daysBeforeDeletion);
+        });
+    }
+
+    private function deleteOldSoftDeletesForModel($modelName, $modelConfig, $daysBeforeDeletion)
+    {
+        $daysBeforeDeletion = empty($modelConfig['days']) ? $daysBeforeDeletion : $modelConfig['days'];
+
+        return DB::table((new $modelName)::getTableName())
+            ->where('deleted_at', '<', Carbon::today()->subDays($daysBeforeDeletion))
+            ->delete();
+    }
+
+    private function logAffectedRows(Collection $deletedRows)
+    {
+        if (! config('quicksand.log', false) || $deletedRows->isEmpty()) {
+            return;
+        }
+
+        Log::info(sprintf(
+            '%s force deleted these number of rows: %s',
+            get_class($this),
+            print_r($deletedRows->all(), true)
+        ));
+    }
+}
