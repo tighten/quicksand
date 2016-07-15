8a53f0c25462296a1afcb3664329f1df7a33f3cb
Benson Lee <benson.lee@gmail.com>
2016-07-15 12:00:12 -0700

    Document configuration file

--- src/config/quicksand.php
+++ src/config/quicksand.php
@@ -1,9 +1,15 @@
 <?php
 
 return [
+    // Days before delete soft deleted content
     'days' => 30,
+
+    // Log the number of soft deleted records per model
     'log' => false,
+
+    // List of models with soft deletes enabled
     'models' => [
+        // Example::class,
         // User::class => [
         //     'days' => '30' // per-model days setting override
         // ]
