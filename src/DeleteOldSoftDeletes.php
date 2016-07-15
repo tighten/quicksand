529644dab628f5d35483a0beeb6988e148f08a70
Benson Lee <benson.lee@gmail.com>
2016-07-15 13:26:03 -0700

    Update namespace

--- src/DeleteOldSoftDeletes.php
+++ src/DeleteOldSoftDeletes.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Quicksand;
+namespace TightenCo\Quicksand;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
--- src/QuicksandServiceProvider.php
+++ src/QuicksandServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Quicksand;
+namespace TightenCo\Quicksand;
 
 use Illuminate\Support\ServiceProvider;
 
