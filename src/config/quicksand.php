629d4fd51b44847807407e76b199dd410c391f71
Matt Stauffer <mattstauffer@users.noreply.github.com>
2016-08-17 16:37:56 -0400

    Clarify comments in publishable config file

--- src/config/quicksand.php
+++ src/config/quicksand.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    // Days before delete soft deleted content
+    // Days before deleting soft deleted content
     'days' => 30,
 
-    // Log the number of soft deleted records per model
+    // Whether to log the number of soft deleted records per model
     'log' => false,
 
-    // List of models with soft deletes enabled
+    // List of models to run Quicksand on
     'models' => [
         // Example::class,
         // User::class => [
